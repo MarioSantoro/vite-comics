40ad41b4c3c13bda818707eb7a78f5d9da58a977
MarioSantoro <mariosantoro124@gmail.com>
2023-06-15 15:42:38 +0200

    created main

---
+++ src/components/AppMain.vue
@@ -0,0 +1,22 @@
+<template>
+    <main class="content-black">
+        <div class=" container">
+            <h1> -- > Content goes here </h1>
+        </div>
+    </main>
+</template>
+<script>
+export default {
+
+}
+</script>
+<style lang="scss" scoped>
+@use "../styles/utilities/mixins.scss" as *;
+@use "../styles/utilities/variables.scss" as *;
+
+main.content-black {
+    background-color: #1c1c1c;
+    color: $colorFontWhite;
+    padding: 2rem 0;
+}
+</style>
