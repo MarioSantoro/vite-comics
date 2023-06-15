0272f18f6d594caea1bc962c91708533ddfc5425
MarioSantoro <mariosantoro124@gmail.com>
2023-06-15 17:27:03 +0200

    create a section merchandise and create a list dynamic

--- src/components/AppMain.vue
+++ src/components/AppMain.vue
@@ -1,22 +1,45 @@
 <template>
-    <main class="content-black">
-        <div class=" container">
-            <h1> -- > Content goes here </h1>
-        </div>
+    <main>
+        <section>
+            <div class="content-black">
+                <div class=" container">
+                    <h1> -- > Content goes here </h1>
+                </div>
+            </div>
+        </section>
+        <section>
+            <div class="content-blue">
+                <AppSectionBlue />
+            </div>
+        </section>
     </main>
 </template>
 <script>
+import AppSectionBlue from './AppSectionBlue.vue';
 export default {
+    data() {
+        return {
 
+        }
+    },
+
+    components: {
+        AppSectionBlue,
+    }
 }
 </script>
 <style lang="scss" scoped>
 @use "../styles/utilities/mixins.scss" as *;
 @use "../styles/utilities/variables.scss" as *;
 
-main.content-black {
+div.content-black {
     background-color: #1c1c1c;
     color: $colorFontWhite;
+    padding: 2rem 0;
+}
+
+div.content-blue {
+    background-color: $colorFontBlue;
     padding: 2rem 0;
 }
 </style>
---
+++ src/components/AppSectionBlue.vue
@@ -0,0 +1,59 @@
+<template>
+    <div class="container">
+        <ul class="flex between">
+            <li v-for="item in listMerch" class="flex centered-flex">
+                <img :src="item.image" alt="Image merch">
+                <p>{{ item.text }}</p>
+            </li>
+        </ul>
+    </div>
+</template>
+<script>
+export default {
+    data() {
+        return {
+            listMerch: [
+                {
+                    image: "../src/assets/img/buy-comics-digital-comics.png",
+                    text: "Digital Comics"
+                },
+                {
+                    image: "../src/assets/img/buy-comics-merchandise.png",
+                    text: "Dc merchandise"
+                },
+                {
+                    image: "../src/assets/img/buy-comics-subscriptions.png",
+                    text: "subscriptions"
+                },
+                {
+                    image: "../src/assets/img/buy-comics-shop-locator.png",
+                    text: "comic shop locator"
+                },
+                {
+                    image: "../src/assets/img/buy-dc-power-visa.svg",
+                    text: "dc power visa"
+                },
+            ]
+        }
+    }
+}
+</script>
+<style lang="scss" scoped>
+@use "../styles/utilities/mixins.scss" as *;
+@use "../styles/utilities/variables.scss" as *;
+
+li {
+    img {
+        width: 55px;
+        height: 55px;
+        object-fit: contain;
+    }
+
+    p {
+        color: $colorFontWhite;
+        text-transform: uppercase;
+        margin-left: 1rem;
+    }
+
+}
+</style>
