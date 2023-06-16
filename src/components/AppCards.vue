b76ad09e48af46573728d21130225ca639d628dc
MarioSantoro <mariosantoro124@gmail.com>
2023-06-16 13:09:07 +0200

    add jumbotron

---
+++ src/components/AppCards.vue
@@ -0,0 +1,10 @@
+<template ></template>
+<script>
+export default {
+
+}
+</script>
+<style lang="scss">
+@use "../styles/utilities/mixins.scss" as *;
+@use "../styles/utilities/variables.scss" as *;
+</style>
